5bdac2876081cc391edef62e2fa9cb7aa3b42e65
devcamilog <kamy.626@hotmail.com>
2023-10-25 15:51:29 -0500

    Se creo la view de login y se modifico en settings.py para buscar las rutas de los templates

--- edificio_administrativo/urls.py
+++ edificio_administrativo/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from edificio_app.views import loginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',loginView),
 ]
--- edificio_app/views.py
+++ edificio_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse , JsonResponse
+
 
 # Create your views here.
+
+def loginView(request):
+    return render (request, 'login.html')
