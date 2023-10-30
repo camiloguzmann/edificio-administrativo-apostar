6d6fdf808f065bf8d52d58965dd73b8f0fafdfa6
devcamilog <kamy.626@hotmail.com>
2023-10-30 13:10:56 -0500

    Se modifico en settings algunos parametros para que a travez del protocolo smtp para el envio de correos , donde se configuro el puerto , el correo host y la contraseña , cabe destacar que ese correo sera provicional

--- edificio_administrativo/settings.py
+++ edificio_administrativo/settings.py
@@ -134,4 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = 'camilo.guzman16@misena.edu.co'  
+EMAIL_HOST_PASSWORD = 'wjhzkkoumriamuxd' 
